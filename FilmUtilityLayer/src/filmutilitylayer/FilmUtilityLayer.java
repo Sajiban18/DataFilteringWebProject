2b8d2f482af364b18d9a886bd10479c5f2a276d4
Sajiban Parameswaran <p.sajiban18@gmail.com>
2017-03-21 14:47:20 +0000

    Add files via upload

---
+++ FilmUtilityLayer/src/filmutilitylayer/FilmUtilityLayer.java
@@ -0,0 +1,40 @@
+package filmutilitylayer;
+
+import filmclasslayer.FilmClassLayer.*;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+
+public class FilmUtilityLayer
+{
+    //static Cache test = CacheData();
+    
+    public static Cache CacheData()
+    {
+        Films filmcacheddata;
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("preConfigured",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, Films.class,
+                                ResourcePoolsBuilder.heap(100))
+                                .build())
+                .build(true);
+        
+            
+            Cache<String, Films> myCache
+                    = cacheManager.getCache("preConfigured", String.class, Films.class);
+            
+        return myCache;
+    }
+    
+    public static void put(Films filmdata, Cache myCache)
+    {
+        myCache.put("preConfigured", filmdata);
+    }
+    
+    public static Films get(Cache myCache)
+    {
+        return (Films)myCache.get("preConfigured");
+    }
+}
